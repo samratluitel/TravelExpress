97b36eaa1b50a358ebc01c90671be100f5a53a2a
samratluitel <ngc2059@gmail.com>
2018-04-15 21:57:47 +0545

    UI slider added - unstyled

---
+++ app.js
@@ -0,0 +1,21 @@
+var slider= tns({
+    "container": ".gallery-slider",
+    "items": 4,
+    "controlsContainer": "#customize-controls",
+    "controls":true,
+    "autoplay": true,
+    "nav":false,
+    "autoplayButtonOutput":false,
+    "autoplayTimeout": 3000,
+    "speed": 400
+})
+var slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 100
+    }
+});
