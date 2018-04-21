aaece954bf2ec92c6112ce70051c8d110ef413dc
samratluitel <ngc2058@gmail.com>
2018-04-21 16:09:27 +0300

    Slider Area completed

--- app.js
+++ app.js
@@ -8,6 +8,65 @@ function CloseSlideMenu(){
     document.getElementById("side-menu").style.width="0px";
 }
 
+
+window.FontAwesomeConfig = {
+    searchPseudoElements: true
+}
+
+
+// Slider Area slider
+var slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 300
+    }
+});
+
+var sliderMin = document.getElementById("slider-min");
+
+var sliderMax = document.getElementById("slider-max");
+
+slider.noUiSlider.on('update', function( values, handle ) {
+
+	var value = values[handle];
+
+	if ( handle ) {
+		sliderMax.textContent = Math.round(value);
+	} else {
+		sliderMin.textContent = Math.round(value);
+	}
+});
+
+sliderMin.addEventListener('change', function(){
+	slider.noUiSlider.set([this.value, null]);
+});
+
+sliderMax.addEventListener('change', function(){
+	slider.noUiSlider.set([null, this.value]);
+});
+// Slider area date picker
+var checkin = new Pikaday({
+    field: document.getElementById('datepicker'),
+    format: 'D/M/YYYY',
+    onSelect: function() {
+        console.log(this.getMoment().format('Do MMMM YYYY'));
+    }
+});
+
+var checkout = new Pikaday({
+    field: document.getElementById('datepicker2'),
+    format: 'D/M/YYYY',
+    onSelect: function() {
+        console.log(this.getMoment().format('Do MMMM YYYY'));
+    }
+});
+
+
+// Gallery Area slider
 var slider= tns({
     "container": ".gallery-slider",
     "items": 4,
@@ -19,14 +78,6 @@ var slider= tns({
     "autoplayTimeout": 3000,
     "speed": 400
 })
-var slider = document.getElementById('slider');
 
-noUiSlider.create(slider, {
-    start: [20, 80],
-    connect: true,
-    range: {
-        'min': 0,
-        'max': 100
-    }
-});
+// Styles to All the select element
 Select.init();
